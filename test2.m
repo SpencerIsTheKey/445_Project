cd4967bb07e722e08079867723a0469c318fc88a
zachcbenny <bardown14@hotmail.com>
2019-03-31 14:24:18 -0700

    Add files via upload

--- test2.m
+++ test2.m
@@ -2,7 +2,7 @@ BG = zeros(1080,1920);
 catit = zeros(1080,1920);
 % black background (my webcam is 480x640) 
 % Segment FG and update background every frame 
-thresh = 0.06; 
+thresh = 0.25; 
 videoReader = vision.VideoFileReader('skihill.mp4'); 
 % threshold for 0..1 grayscale 
 s = strel('disk',4);
@@ -14,15 +14,20 @@ while ~isDone(videoReader)
     
     FG_mask = diff > thresh; 
     FG = bwframe .* FG_mask; 
+    FG = imopen(FG,s);
     % Uncomment ONLY ONE of the imshow 
-    %imshow(BG);        
+    %only moving
+    FGmove = FG <0.5 & FG>0;
+    %imshow(FGmove);        
     % show background being slowly updated 
+    [a,b] = bwlabel(FGmove);
+    props = regionprops(a);
+    frame(:,:,1) = (frame(:,:,1)+FGmove);
+    imshow(frame);
     
-    frame(:,:,1) = (frame(:,:,1)+FG_mask);
-    imshow(frame);   
     % show mask with moving object only 
     %imshow(FG);  
     % show moving objects 
     % update BG 
-    BG = 0.95 * BG + 0.05 * bwframe; 
+    BG = 0.99 * BG + 0.01 * bwframe; 
 end
